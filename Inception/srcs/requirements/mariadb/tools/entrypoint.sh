a53747658200bd5bf06b6a22e87f942e02550ee4
agaroux <agaroux@Inception.myguest.virtualbox.org>
2026-01-29 15:19:52 +0100

    v1

---
+++ Inception/srcs/requirements/mariadb/tools/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Start MariaDB in the background
+mysqld_safe &
+
+# Wait for MariaDB to be available
+until mysqladmin ping --silent; do
+    echo "Waiting for MariaDB to be available..."
+    sleep 2
+done
+
+echo "MariaDB is ready!"
+
+# Run initialization script with environment variable substitution
+if [ -f /tools/init.sql ]; then
+    envsubst < /tools/init.sql | mysql -u root -p"$SQL_ROOT_PASSWORD"
+    echo "init.sql executed."
+else
+    echo "init.sql not found."
+fi
+
+# Keep the container running
+wait
---
+++ Inception/srcs/requirements/wordpress/conf/auto_config.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+# Wait for MariaDB to be ready
+until mysqladmin ping -h"mariadb" --silent; do
+    echo "Waiting for MariaDB..."
+    sleep 2
+done
+echo "MariaDB is ready!"
+
+if [ ! -f wp-config.php ]; then
+    wp config create    --allow-root \
+                        --dbname=$SQL_DATABASE \
+                        --dbuser=$SQL_USER \
+                        --dbpass=$SQL_PASSWORD \
+                        --dbhost=mariadb:3306 --path='/var/www/wordpress'
+fi
+
+if ! wp core is-installed --allow-root --path='/var/www/wordpress'; then
+    wp core install --allow-root \
+        --url="$WP_URL" \
+        --title="$WP_TITLE" \
+        --admin_user="$WP_ADMIN" \
+        --admin_password="$WP_ADMIN_PASSWORD" \
+        --admin_email="$WP_ADMIN_EMAIL" \
+        --path='/var/www/wordpress'
+
+    wp user create "$WP_USER" "$WP_USER_EMAIL" \
+        --user_pass="$WP_USER_PASSWORD" \
+        --role=author \
+        --allow-root \
+        --path='/var/www/wordpress'
+fi
+
+if [ ! -d /run/php ]; then
+    mkdir /run/php
+fi
+
+exec /usr/sbin/php-fpm8.2 -F -R
